d2f0b9b029bde4124bf293782cec5d53b300257b
kolterdyx <kolterdyx@localhost.localdomain>
2020-07-23 16:39:27 +0100

    Uploading final format (0.0.1)

--- 0.0.1/Windows/VoidShips/scripts/GUI/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.insert(0, "scripts/GUI")
-from entry import *
-from gui import *
-from menu import *
-from button import *
--- 0.0.1/Windows/VoidShips/scripts/__init__.py
+++
@@ -1,5 +0,0 @@
-import sys
-sys.path.insert(0, "scripts/")
-from camera import *
-from world_manager import *
-from GUI import *
