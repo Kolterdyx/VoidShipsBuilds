474f45b3797bcde02ace37f72d69005319dc59f8
kolterdyx <kolterdyx@localhost.localdomain>
2020-07-23 16:03:04 +0100

    Uploading version 0.0.1 in a different format

--- 0.0.1/Linux/VoidShips/objects/PhysicObjects/__init__.py
+++
@@ -1,5 +0,0 @@
-import sys
-sys.path.insert(0, "objects/PhysicObjects/")
-from KinematicBody import *
-from StaticBody import *
-from RigidBody import *
--- 0.0.1/Linux/VoidShips/objects/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.insert(0, "objects/")
-from blocks import *
-from enemy import *
-from item import *
-from player import *
